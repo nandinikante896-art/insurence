3bda3eadab16c40c12d3bad664fee9613a31dba5
nandinikante896-art <nandinikante896@gmail.com>
2026-03-11 13:03:13 +0530

    Refactor predict function to simplify output rounding

--- app.py
+++ app.py
@@ -4,31 +4,26 @@ import numpy as np
 
 app = Flask(__name__)
 
-# load model
 model = joblib.load("insurance_model.pkl")
 
-@app.route('/')
+@app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route('/predict', methods=['POST'])
+@app.route("/predict", methods=["POST"])
 def predict():
-
-    age = int(request.form['age'])
-    sex = int(request.form['sex'])
-    bmi = float(request.form['bmi'])
-    children = int(request.form['children'])
-    smoker = int(request.form['smoker'])
-    region = int(request.form['region'])
+    age = int(request.form["age"])
+    sex = int(request.form["sex"])
+    bmi = float(request.form["bmi"])
+    children = int(request.form["children"])
+    smoker = int(request.form["smoker"])
+    region = int(request.form["region"])
 
     features = np.array([[age, sex, bmi, children, smoker, region]])
-
     prediction = model.predict(features)
 
-    output = round(prediction[0],2)
-
     return render_template("index.html",
-                           prediction_text="Predicted Insurance Charges: $" + str(output))
+                           prediction_text="Predicted Insurance Charges: $" + str(round(prediction[0],2)))
 
 if __name__ == "__main__":
     app.run(debug=True)
